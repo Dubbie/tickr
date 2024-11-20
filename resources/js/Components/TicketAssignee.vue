b3d5e2df3087210c8168067249514df910fa2b43
Daniel Miho <miho.dnl@gmail.com>
2024-11-20 11:44:39 +0100

    fix ticket assignee invisible in dark ode

--- resources/js/Components/TicketAssignee.vue
+++ resources/js/Components/TicketAssignee.vue
@@ -16,7 +16,7 @@ defineProps({
         <div class="flex items-center space-x-2 text-xs">
             <template v-if="!assignee">
                 <div
-                    class="size-6 rounded-md border border-dashed border-zinc-900/40 bg-transparent"
+                    class="size-6 rounded-md border border-dashed border-zinc-900/40 bg-transparent dark:border-white/20"
                 ></div>
 
                 <p class="text-zinc-500">Nobody</p>
